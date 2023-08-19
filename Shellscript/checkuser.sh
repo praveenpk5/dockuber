06069e03929d5116c89b75d34071e4ec50758ea1
Praveen <praveenmpk36@gmail.com>
2023-08-19 21:42:46 +0530

    file

---
+++ Shellscript/checkuser.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+USERID = $ (id -u)
+if [$USERID -ne 0]; then
+echo "you need to root user to execute this script"
+exit 1
+fi
+
+yum install nginx -y
+if [$? -ne 0]; then
+echo "installing nginx is failed"
+exit
+else
+echo "installing nginx is success"
+fi
