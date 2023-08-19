17bc4bbdb49dc23bc907551e0aaad3d937cde9ba
Praveen <praveenmpk36@gmail.com>
2023-08-19 20:29:07 +0530

    file

--- Shellscript/multval.sh
+++ Shellscript/multval.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "enter the values:"
 read numbers
-IFS =' 'read -r -a array <<< "$numbers"
+IFS ' ' read -r -a array <<< "$numbers"
 echo "you entered values are:"
 for numbers in "${array[@]}"
 do
