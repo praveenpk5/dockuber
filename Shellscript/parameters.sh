f47daf55de59594dbfeef5a95ddc073aee524694
Praveen <praveenmpk36@gmail.com>
2023-08-20 10:46:08 +0530

    added

--- Shellscript/parameters.sh
+++ Shellscript/parameters.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 Hello() {
     echo "Hell !!! $1"
-    echo "script name: $0"
-    echo "number of args: $#"
-    echo "all args are: $@"
+    echo "script name:$0"
+    echo "number of args:$#"
+    echo "all args are:$@"
 }
 
-echo "before calling function, checking name: $1"
-echo "number of args: $#"
-echo "all args are: $@"
+echo "before calling function, checking name:$1"
+echo "number of args:$#"
+echo "all args are:$@"
 Hello $1
